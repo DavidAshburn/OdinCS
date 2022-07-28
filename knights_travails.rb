a0102739d901fe85085a45359cbba7076ae15420
DavidAshburn <oneinchwebbing@gmail.com>
2022-07-28 13:01:24 -0700

    rewrote, knights completed, requires edits for interaction

--- knights_travails.rb
+++ knights_travails.rb
@@ -1,130 +1,101 @@
-
-class Node
-	attr_accessor :x, :y, :parent, :child_list, :route
-	def initialize(x = 0,y = 0, parent = nil)
-		@x = x
-		@y = y
-		@parent = parent
-		@child_list = []
-		@route = []
-	end
-end
-
 class Travails
-	attr_accessor :board, :root, :routes
-	def initialize(square)
+	attr_accessor :board, :root
+	def initialize(start_x, start_y)
+		@root = Node.new(start_x, start_y)
 		@board = Array.new(8) { Array.new(8, false) }
-		@routes = Array.new { Array.new(8, nil) }
-		@root = Node.new(square[0], square[1])
-		build_tree(@root)
+		@queue = [@root]
+		build_tree
 	end
 
-	def knight_moves(x,y) #selects only valid moves that haven't been found yet
-		a = [x+2,y+1]
-		b = [x+1,y+2]
-		c = [x-2,y+1]
-		d = [x-1,y+2]
-		e = [x+2,y-1]
-		f = [x+1,y-2]
-		g = [x-2,y-1]
-		h = [x-1,y-2]
-		list = [a,b,c,d,e,f,g,h]
-		good_moves = []
-		list.each do |i| #filter invalid moves and old squares
-			x = i[0]
-			y = i[1]
-			good_moves.push(i) unless x > 7 || x < 0 || y > 7 || y < 0 || @board[x][y]
+	def knight_moves(start_x, start_y) #returns array of valid, new squares to move to format [x,y]
+		a = [start_x + 1, start_y + 2]
+		b = [start_x + 2, start_y + 1]
+		c = [start_x - 1, start_y + 2]
+		d = [start_x - 2, start_y + 1]
+		e = [start_x + 1, start_y - 2]
+		f = [start_x + 2, start_y - 1]
+		g = [start_x - 1, start_y - 2]
+		h = [start_x - 2, start_y - 1]
+
+		candidates = [a,b,c,d,e,f,g,h]
+		valids = candidates.reduce([]) do |valids, candidate| #clear visited and invalid squares @board tracks visited squares
+			if (candidate[0] < 8 && candidate[0] >= 0 && candidate[1] < 8 && candidate[1] >= 0 && @board[candidate[0]][candidate[1]] == false)
+				valids.push(candidate)
+			end
+			valids
 		end
-		good_moves.each do |i| #mark off newly visited squares
-			x = i[0]
-			y = i[1]
-			@board[x][y] = true
-		end 
-		good_moves
+		valids.each { |pair| @board[pair[0]][pair[1]] = true }
+		return valids
 	end
 
-	def chess(list) #converts [(0..7),(0..7)] into a string like 'A4'
-		moves = []
-		list.each do |i|
-			string = (i[0] + 'A'.ord).chr
-			string.concat((i[1] + '1'.ord).chr)
-			moves.push(string)
+	def one_from_queue
+		new_moves = knight_moves(@queue[0].x_coord, @queue[0].y_coord)
+		init_nodes = []
+		new_moves.each do |x|
+			init_nodes.push(Node.new(x[0], x[1]))
 		end
-		puts moves
-	end
-
-	def chess_square(string) #converts a string like 'A4' into [(0..7),(0..7)]
-		indexes = string.split('')
-		col = indexes[0].upcase.ord - 'A'.ord
-		row = indexes[1].to_i - 1
-		return [col,row]
-	end
-
-	def build_tree(node)
-		list = knight_moves(node.x,node.y)
-		return if list.length == 0
-
-		node.child_list = list.reduce([]) do 
-			|reduction, x| reduction.push(Node.new(x[0],x[1],node))
+		init_nodes.each do |x| #this will copy the parent routes, add the parent, and assign to each child, then add children to queue and to parent
+			this_route = []
+			@queue[0].route.each { |x| this_route.push(x) }
+			this_route.push([@queue[0].x_coord, @queue[0].y_coord])
+			x.route = this_route
+			@queue.push(x)
+			@queue[0].children.push(x)
 		end
-		if node.child_list.length > 0
-			node.child_list.each do |x| 
-				build_tree(x)
+		@queue.shift
+	end
+
+	def get_route(list)
+		line = [@root]
+		
+		while line.length > 0
+			if line[0].x_coord == list[0] && line[0].y_coord == list[1]
+				line[0].route.push([list[0],list[1]])
+				return line[0].route
+			else
+				line[0].children.each { |x| line.push(x) }
+				line.shift
 			end
 		end
+		return "oops"
 	end
 
-	def level_order(queue = [@root]) #populates @routes if no block given
-    	count = 1
-    	dex = 0
-    	while(count < 65)
-    		queue[dex].child_list.each do |child|
-    			queue.push(child)
-    			count += 1
-    		end
-    		dex += 1
-    	end
+	def build_tree
+		while @board.flatten.all? == false
+			one_from_queue
+		end
+	end
 
-    	if block_given?
-        	queue.each { |val| yield(val) } 
-    	else
-    		queue.each do |val|
-    			val.route = trace(val)
-    		end
-    	end
-  	end
+	def coord_to_chess(pair) #returns string like 'A4'
+		return "#{(pair[0] + 'A'.ord).chr}#{(pair[1] + '1'.ord).chr}"
+	end
 
-  	def trace(node)
-  		list = []
-  		trace_helper(node,list)
-  		return list
-  	end
+	def chess_to_coord(string) #returns [x,y] coords (0..7)
+		pair = string.split('')
+		x = pair[0].ord - 'A'.ord
+		y = pair[1].ord - '1'.ord
+		return [x,y]
+	end
 
-  	def trace_helper(node,list)
-  		list.push([node.x, node.y])
-  		if node.parent != nil
-  			trace_helper(node.parent,list)
-  		end
-  		return
-  	end
-
-  	def get_route(pair)
-  		level_order do |node|
-  			if node.x == pair[0] && node.y == pair[1]
-  				return node.route
-  			end
-  		end
- 	end
+	def input_square(string)
+		pair = chess_to_coord(string)
+		output = get_route(pair)
+		output.each do |x|
+			puts coord_to_chess(x)
+		end
+	end
 end
 
-
-
-game = Travails.new([4,4])
-
-game.level_order
-
-game.get_route([3,3]).each do |val|
-	game.chess(val)
+class Node
+	attr_accessor :x_coord, :y_coord, :children, :route
+	def initialize(x_coord = nil, y_coord = nil, route = [])
+		@x_coord = x_coord
+		@y_coord = y_coord
+		@route = route
+		@children = []
+	end
 end
-p game.get_route([3,3])
 
+game = Travails.new(3,3)
+
+game.input_square("C4")
